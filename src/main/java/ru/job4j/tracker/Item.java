06b1d589013b54dbdcab2bbe8529f7739c085a67
Николай <nikolaymishakin@yandex.ru>
2021-04-05 19:42:45 +0300

    6. Date. Отображение даты. [#498447]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
-
+    LocalDateTime created = LocalDateTime.now();
     public Item() {
     }
 
@@ -14,6 +16,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getTime() {
+        return created;
     }
 
     public int getId() {
