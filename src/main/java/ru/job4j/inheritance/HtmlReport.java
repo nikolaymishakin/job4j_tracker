155b14c51ddb598084f366271662dea85220446d
Николай <nikolaymishakin@yandex.ru>
2021-04-05 19:57:39 +0300

    4. Переопределение [#498445]

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -1,7 +1,7 @@
 package ru.job4j.inheritance;
 
 public class HtmlReport extends TextReport {
-    @Override
+    @Overrid
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>" +
                 "<br/>" +
