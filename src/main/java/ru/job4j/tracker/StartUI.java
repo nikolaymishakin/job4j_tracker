886ef16174b38eca45b6f85d2b37e65208a8a730
Николай <nikolaymishakin@yandex.ru>
2021-04-06 16:35:38 +0300

    6. Date. Отображение даты. [#498447]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,7 +4,7 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.created.format(formatter);
+        String currentDateTimeFormat = item.getTime().format(formatter);
         System.out.println(currentDateTimeFormat);
     }
 }
