9c3f87223e6613a2ad922ea673d8721e5d8fb121
Николай <nikolaymishakin@yandex.ru>
2021-09-21 00:01:25 +0300

    Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,4 +56,15 @@ public class Tracker {
         }
         return false;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            System.arraycopy(items, index + 1, items, index,size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return result;
+    }
 }
