f1871df6fb435d3eadd9f160021fe4f7b60ea1cb
Николай <nikolaymishakin@yandex.ru>
2021-09-22 16:14:01 +0300

    Приведение типов. Повышение типа и понижение типа. [#181322]

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -3,7 +3,7 @@ package ru.job4j.oop;
 public class College {
     public static void main(String[] args) {
         Freshman freshman = new Freshman();
-        Student student = (Student) freshman; // Приведение типа за счет понижения по иерархии
+        Student student = freshman; // Повышающее приведение типа
         Object obj = freshman; // Повышающее приведение типа
     }
 }
--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,6 +1,6 @@
 package ru.job4j.oop;
 
-public class Student extends Freshman {
+public class Student {
     public void song() {
         System.out.println("I believe I can fly");
     }
