7bcb36aa29b3940e71a832babcf03567912d6443
Николай <nikolaymishakin@yandex.ru>
2021-09-25 22:55:20 +0300

    Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class ElementNotFoundException extends Exception {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; ++i) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(indexOf(new String[]{"Иванов", "Иван","Иваныч"}, "Ива"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
