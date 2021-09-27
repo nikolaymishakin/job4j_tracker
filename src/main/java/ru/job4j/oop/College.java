348b15d5b1ff2679b6e40c1efccd38b4afbf7f1a
Николай <nikolaymishakin@yandex.ru>
2021-09-27 18:36:17 +0300

    исправлено

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -2,10 +2,6 @@ package ru.job4j.oop;
 
 public class College {
     public static void main(String[] args) {
-        /**
-         * Student student = freshman;  Повышающее приведение типа
-         *         Object obj = freshman;  Повышающее приведение типа
-         */
         Freshman freshman = new Freshman();
         Student student = freshman;
         Object obj = freshman;
